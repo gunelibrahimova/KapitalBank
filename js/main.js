019c44c2dffa0b7a91ee93023e566d950bd62807
gunelibrahimova <75317328+gunelibrahimova@users.noreply.github.com>
2022-05-07 13:25:45 +0400

    07.05.2022

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var header = document.querySelector("#header");
+var top = document.querySelector(".top");
+var searchPage = document.querySelector(" .searchPage");
+var searchIcon = document.querySelector(".searchButton") ;
+
+
+searchIcon.onclick = function(){
+    top.style.display = "none"
+    // searchPage.style.display = "block"  
+}
