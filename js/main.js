098840b357519a568fd0cff1af215405a86202c0
gunelibrahimova <75317328+gunelibrahimova@users.noreply.github.com>
2022-05-10 22:53:50 +0400

    10.05.2022

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 var header = document.querySelector("#header");
-var top = document.querySelector(".top");
-var searchPage = document.querySelector(" .searchPage");
+var top = document.getElementById("#topheader");
+var searchPage = document.querySelector(".searchPage");
 var searchIcon = document.querySelector(".searchButton") ;
-
+var markIcon = document.querySelector(".mark")
 
 searchIcon.onclick = function(){
-    top.style.display = "none"
-    // searchPage.style.display = "block"  
+    document.getElementById("myDIV").style.display = "none";
+    searchPage.style.display = "block"  
 }
+
+markIcon.onclick = function(){
+    document.getElementById("myDIV").style.display = "block";
+    searchPage.style.display = "none    "  
+}   
+
+
+
+
